9444bf54bb8d537ed65381239b6041fb84e0d25a
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-11-12 00:15:37 +0300

    Добавьте файлы проекта.

---
+++ DataBase_ADO5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataBase_ADO5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
